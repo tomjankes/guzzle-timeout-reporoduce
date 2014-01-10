1fee4e455af947d79b153eeefe60508da6a8b669
Tomasz Jankowski <t.jankowski@allegro.pl>
2014-01-10 11:26:19 +0100

    Endpoint

--- test_endpoint.php
+++ test_endpoint.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Put this file in document root
+ *
+ * Simulates web service that takes long to respond
+ */
+
 $timeout_in_miliseconds = 2000;
 usleep($timeout_in_miliseconds * 1000);
 echo "Done.";
