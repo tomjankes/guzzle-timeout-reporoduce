20e221842be0dbea193a8e759ad231cd532efd06
Tomasz Jankowski <t.jankowski@allegro.pl>
2014-01-10 11:25:04 +0100

    Modifications of curl

--- curl.php
+++ curl.php
@@ -8,6 +8,7 @@ curl_setopt($ch, CURLOPT_NOSIGNAL, true);
 curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 200);
 curl_setopt($ch, CURLOPT_TIMEOUT_MS, 300);
 
+$time = microtime(true);
 $result = curl_exec($ch);
 
 $curl_errno = curl_errno($ch);
@@ -17,6 +18,7 @@ curl_close($ch);
 if ($curl_errno > 0) {
     echo "cURL Error ($curl_errno): $curl_error\n";
 } else {
-    echo "Data received: $data\n";
+    echo "No timeout\n";
 }
+echo "It took: ".(microtime(true) - $time).PHP_EOL;
 ?>
